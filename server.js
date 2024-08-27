f6a3ae2a7ae3f97281f5b8a550adb86953c269d7
misaeltoa <23400025@alumno.utc.edu.mx>
2024-08-26 20:54:37 -0600

    Update CORS configuration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
@@ -8,8 +8,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+const corsOptions = {
+    origin: 'https://piezasfront.onrender.com/', // Reemplaza con la URL de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  };
+  
+  app.use(cors(corsOptions));
+  
 
 // Conexión a MongoDB Atlas
 const uri = process.env.MONGODB_URI;
